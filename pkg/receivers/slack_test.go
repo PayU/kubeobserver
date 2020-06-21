4156e2773eb5ae9dd384ee258a8c9b7a25821543
Jacob Goldenberg <jacob.goldenberg@zooz.com>
2020-06-21 11:10:38 +0300

    fix basic testing flow

--- pkg/receivers/slack_test.go
+++ pkg/receivers/slack_test.go
@@ -4,43 +4,32 @@ import (
 	"errors"
 	"testing"
 	"time"
-	"fmt"
 
 	"github.com/slack-go/slack"
 )
 
 type slackClient interface {
-	HandleEvent(ReceiverEvent, chan error)
-	PostMessage(string, slack.MsgOption)
+	postMessage(string, slack.MsgOption)
 }
 
 type MockSlackClient struct{}
 
 func (m *MockSlackClient) postMessage(ch string, opt slack.MsgOption) (string, time.Time, error) {
-	return mockPostMessage(ch, slack.MsgOptionAsUser(true))
-}
-
-func mockPostMessage(ch string, opt slack.MsgOption) (string, time.Time, error) {
 	return ch, time.Now(), nil
 }
 
 func (m *MockSlackClient) handleEvent(e ReceiverEvent, c chan error) {
-	mockHandleEvent(e, c)
-}
-
-func mockHandleEvent(e ReceiverEvent, c chan error) {
 	text := e.EventName + "" + e.Message
 
 	attach := slack.Attachment{
 		Text: text,
 	}
 
-	err, _, _ := mockPostMessage("mockChannel", slack.MsgOptionAttachments(attach))
+	err, _, _ := m.postMessage("mockChannel", slack.MsgOptionAttachments(attach))
 
 	if err != "mockChannel" {
 		c <- errors.New("problem with mockPostMessage")
 	}
-
 }
 
 func TestPostMessage(t *testing.T) {
@@ -59,11 +48,11 @@ func TestHandleEvent(t *testing.T) {
 	event := ReceiverEvent{EventName: "mockEvent", Message: "mockMessage", AdditionalInfo: make(map[string]interface{})}
 	channel := make(chan error)
 
-	fmt.Println("starting handle event")
 	client.handleEvent(event, channel)
-	fmt.Println("finished handle event")
 
-	if err := <-channel; err != nil {
-		t.Error("error")
+	select {
+	case err := <-channel:
+		t.Error(err)
+	default:
 	}
 }
