4502af1a07c60ce5ef8e88c094958cc0d87a8acb
shaimoria <shai.moria@zooz.com>
2020-06-21 18:10:19 +0300

    remove unused code

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -2,6 +2,8 @@ echo "***************** Running Travis.ci before script *****************"
 export VERSION=$(cat $PWD/version)
 export DOCKER_IMAGE=zooz/kubeobserver:$VERSION
 
+reportVars
+
 function reportVars() {
     echo "DOCKER_IMAGE: $DOCKER_IMAGE"
     echo "VERSION: $VERSION"
