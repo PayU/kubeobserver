0921c012d6d36521b0463bc2f9ecb9a23f6a5f96
Jacob Goldenberg <jacob.goldenberg@zooz.com>
2020-06-23 18:34:05 +0300

    test handler with mux

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -1,42 +1,21 @@
 package server
 
 import (
-	"context"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
-	"os"
-	"os/signal"
-	"strings"
 	"testing"
-	"time"
 
-	"github.com/PayU/kubeobserver/pkg/controller"
-	"github.com/rs/zerolog/log"
+	"github.com/shyimo/kubeobserver/pkg/server"
 )
 
 func TestHealthHandler(t *testing.T) {
-	recorder := httptest.NewRecorder()
-	r := http.Request{}
+	mux := http.NewServeMux()
+	mux.Handle("/health", http.HandlerFunc(server.HealthHandler))
 
-	go controller.StartWatch(time.Now())
+	srv := &http.Server{
+		Handler: mux,
+	}
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	HealthHandler(recorder, &r)
-	_, cancel := context.WithCancel(context.Background())
-
-	go func() {
-		oscall := <-c
-		log.Info().Msg(fmt.Sprintf("system call:%s", oscall))
-		cancel()
-	}()
-
-	response := recorder.Result()
-	body, err := ioutil.ReadAll(response.Body)
-
-	if !strings.Contains(string(body), "{\"is_healthy\":false,\"is_pod_controller_sync\":false}") || err != nil {
-		t.Error("TestHealthHandler: Health handler hasn't responded correctly")
+	if srv == nil {
+		t.Error("TestHealthHandler: couldn't use HealthHandler in http server")
 	}
 }
