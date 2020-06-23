1f51fb467491f527b94298c2006d9b970e4f281a
Jacob Goldenberg <jacob.goldenberg@zooz.com>
2020-06-23 15:43:39 +0300

    podWatcher tests

--- pkg/controller/podWatcher_test.go
+++ pkg/controller/podWatcher_test.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
 
 	v1 "k8s.io/api/core/v1"
@@ -42,17 +43,44 @@ func TestPodEventsHandler(t *testing.T) {
 }
 
 func TestGetStateChangeOfContainers(t *testing.T) {
+	cs1 := v1.ContainerStatus{Name: "mockContainer1", Ready: false, RestartCount: 5}
+	cs2 := v1.ContainerStatus{Name: "mockContainer2", Ready: true, RestartCount: 0}
+	s1 := []v1.ContainerStatus{cs1}
+	s2 := []v1.ContainerStatus{cs2}
 
+	results := getStateChangeOfContainers(s1, s2)
+
+	if results == nil {
+		t.Error("TestGetStateChangeOfContainers: couldn't parse containers status arrays")
+	} else {
+		fmt.Println(results)
+	}
 }
 
 func TestParseContainerState(t *testing.T) {
+	csw := v1.ContainerStateWaiting{Reason: "mockWaitingReason", Message: "mockWaitingMessage"}
+	cs := v1.ContainerState{Waiting: &csw}
+	result := parseContainerState(cs)
 
+	if result == "" {
+		t.Error("TestParseContainerStates: couldn't parse container state")
+	} else {
+		fmt.Println(result)
+	}
 }
 
 func TestShouldWatchPod(t *testing.T) {
+	shouldWatch := shouldWatchPod("mockPod")
 
+	if reflect.TypeOf(shouldWatch).Kind() != reflect.Bool || shouldWatch != true {
+		t.Error("TestShouldWatchPod: test hasn't evaluated correctly watch status of mockPod")
+	}
 }
 
 func TestIsSPodControllerSync(t *testing.T) {
+	hasSynced := IsSPodControllerSync()
 
+	if reflect.TypeOf(hasSynced).Kind() != reflect.Bool || hasSynced != false {
+		t.Error("TestIsSPodControllerSync: test hasn't evaluated correctly sync status of unexistent controller")
+	}
 }
