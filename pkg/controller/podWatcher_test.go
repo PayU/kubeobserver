36a4fc9ec5a36d89092c4bb60978bd78cde1dbb5
Jacob Goldenberg <jacob.goldenberg@zooz.com>
2020-06-23 12:26:55 +0300

    finish fw for testing

--- pkg/controller/podWatcher_test.go
+++ pkg/controller/podWatcher_test.go
@@ -1,1 +1,27 @@
 package controller
+
+import "testing"
+
+func TestNewPodController(t *testing.T) {
+
+}
+
+func TestPodEventsHandler(t *testing.T) {
+
+}
+
+func TestGetStateChangeOfContainers(t *testing.T) {
+
+}
+
+func TestParseContainerState(t *testing.T) {
+
+}
+
+func TestShouldWatchPod(t *testing.T) {
+
+}
+
+func TestIsSPodControllerSync(t *testing.T) {
+
+}
---
+++ pkg/server/server_test.go
@@ -0,0 +1,9 @@
+package server 
+
+import (
+	"testing"
+)
+
+func TestHealthHandler(t *testing.T) {
+	
+}
