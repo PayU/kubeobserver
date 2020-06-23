507b97ebfc80f1a91cefad1154b3caf3f914ab2c
Jacob Goldenberg <jacob.goldenberg@zooz.com>
2020-06-23 17:20:44 +0300

    halth handler test

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -1,9 +1,35 @@
-package server 
+package server
 
 import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"os/signal"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/PayU/kubeobserver/pkg/controller"
 )
 
 func TestHealthHandler(t *testing.T) {
-	
+	// start k8s controller watchers
+	go controller.StartWatch(time.Now())
+
+	// create a channel for listening to OS signals
+	// and connecting OS interrupts to the channel.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	recorder := httptest.NewRecorder()
+	r := http.Request{}
+
+	HealthHandler(recorder, &r)
+	response := recorder.Result()
+	body, _ := ioutil.ReadAll(response.Body)
+
+	if !strings.Contains(string(body), "{\"is_healthy\":false,\"is_pod_controller_sync\":false}") {
+		t.Error("TestHealthHandler: Health handler hasn't responded correctly")
+	}
 }
