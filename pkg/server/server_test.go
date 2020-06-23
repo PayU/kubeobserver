ec7426961abfc4bff3afbe9c39f97af33df420df
Jacob Goldenberg <jacob.goldenberg@zooz.com>
2020-06-23 17:44:25 +0300

    fix test

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -14,22 +14,19 @@ import (
 )
 
 func TestHealthHandler(t *testing.T) {
-	// start k8s controller watchers
-	go controller.StartWatch(time.Now())
-
-	// create a channel for listening to OS signals
-	// and connecting OS interrupts to the channel.
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-
 	recorder := httptest.NewRecorder()
 	r := http.Request{}
 
-	HealthHandler(recorder, &r)
-	response := recorder.Result()
-	body, _ := ioutil.ReadAll(response.Body)
+	go controller.StartWatch(time.Now())
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
 
-	if !strings.Contains(string(body), "{\"is_healthy\":false,\"is_pod_controller_sync\":false}") {
+	HealthHandler(recorder, &r)
+
+	response := recorder.Result()
+	body, err := ioutil.ReadAll(response.Body)
+
+	if !strings.Contains(string(body), "{\"is_healthy\":false,\"is_pod_controller_sync\":false}") || err != nil {
 		t.Error("TestHealthHandler: Health handler hasn't responded correctly")
 	}
 }
