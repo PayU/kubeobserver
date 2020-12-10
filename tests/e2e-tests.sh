7936216792021ac793e14f4db102f71ab32882e1
shaimoria <shai.moria@zooz.com>
2020-12-10 14:38:59 +0200

    writing end2end tests

---
+++ tests/e2e-tests.sh
@@ -0,0 +1,26 @@
+export PORT=8080
+export LOG_LEVEL=INFO
+
+echo "************* VERIFY CLEAN ENVIRONMENT *************"
+pInfo=$(sudo lsof -nP -iTCP:$PORT | grep LISTEN)
+if [ -z ${pInfo+x} ]; then 
+    echo "Environment is clean"
+else
+    echo "removing old kubeobserver process"
+    pInfoArray=($pInfo)
+    kill -9 ${pInfoArray[1]}
+fi
+echo "****************************************************"
+
+echo "************* BUILDING & RUNNING KUBEOBSERVER *************"
+make build
+
+./kubeobserver &
+echo "***********************************************************"
+
+echo "Main process will now sleep for 7 seconds..."
+sleep 7
+
+echo "******************* RUNNING TESTS *********************"
+
+echo "************* TESTS FINISHED SUCCESSFULLY *************"
